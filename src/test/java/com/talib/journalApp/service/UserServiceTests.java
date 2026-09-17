44164c1d9b2e4b901fae07f7d155e537fd77a0b2
Talib3102 <talibmohammadnaqiansari@gmail.com>
2026-09-18 00:28:24 +0530

    clearing some Test case code and improve readability of code and improve Spelling mistakes

--- src/test/java/com/talib/journalApp/service/UserServiceTests.java
+++ src/test/java/com/talib/journalApp/service/UserServiceTests.java
@@ -13,42 +13,30 @@ import org.springframework.boot.test.context.SpringBootTest;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
-public class UserServiceTest {
+public class UserServiceTests {
+
     @Autowired
     private UserRepository userRepository;
+
     @Autowired
-    private  UserService userService;
+    private UserService userService;
+
+
     @Disabled
     @ParameterizedTest
-    @ArgumentsSource(UserArgumentProvider.class)
-    public void testSaveNewUser(User user){
+    @ArgumentsSource(UserArgumentsProvider.class)
+    public void testSaveNewUser(User user) {
         assertTrue(userService.saveNewUser(user));
     }
-//    @Disabled
-    //@Test
-    @ParameterizedTest
-//    @CsvSourc
-    //@EnumSource
-      @ValueSource(strings = {
-            "Talib",
-            "Sufiyan",
-            "Sakina"
-    })
-    public void testFindByUserName(String name){
-        assertNotNull(userRepository.findByUserName(name));
-        User user = userRepository.findByUserName("Sufiyan");
-        assertTrue(!user.getJournalEntries().isEmpty());
-        assertNotNull(userRepository.findByUserName("Sufiyan"));
-    }
+
+    @Disabled
     @ParameterizedTest
     @CsvSource({
             "1,1,2",
-            "2,2,4",
-            "3,4,7"
+            "2,10,12",
+            "3,3,9"
     })
-
-
-    public void test(int a,int b,int expected){
-        assertEquals(expected,a+b);
+    public void test(int a, int b, int expected){
+        assertEquals(expected, a + b);
     }
 }
